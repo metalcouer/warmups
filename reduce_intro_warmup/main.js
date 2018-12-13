5066d927a9dc3926b9df493f45db50c83c238ffb
Diane <metalcouer@gmail.com>
2018-12-13 10:22:54 -0700

    inserted solutions

--- reduce_intro_warmup/main.js
+++ reduce_intro_warmup/main.js
@@ -1,22 +1,30 @@
 
 //add all elements together
 function sum(arr){
-
+  return arr.reduce((acc,cur) => acc + cur, 0)
+  
 }
 
 // multiply all elements together
-function multiply(arr){
-
+function multiply(arr){ 
+  return arr.reduce((acc,cur) => acc * cur, 1)
+  
 }
 
 //double each element using reduce
 function double(arr){
-
+  return arr.map(element => element * 2)
+  
 }
 
 // //count the letters
 function countLetters(str){
-
+  let string = str.split('')
+  console.log("string", string)
+  return string.reduce((prev,cur) => {
+    prev[cur] = (prev[cur] || 0) + 1
+    return prev
+  }, {})
 }
 
 module.exports = {
